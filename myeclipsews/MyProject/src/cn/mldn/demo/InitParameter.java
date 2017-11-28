e0cad278ff18da096b1a7f83f77a9b8883ec8df7
dmgfhc <44460588@qq.com>
2017-11-28 18:55:04 +0800

    20171128

---
+++ myeclipsews/MyProject/src/cn/mldn/demo/InitParameter.java
@@ -0,0 +1,15 @@
+package cn.mldn.demo;
+
+public class InitParameter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int x =0;x<args.length;x++) {
+			
+			System.out.println(args[x]);
+		}
+
+	}
+
+}
