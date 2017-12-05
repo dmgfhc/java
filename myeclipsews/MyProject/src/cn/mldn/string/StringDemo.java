6c05e8f65ed17d8e5c33730a3ab341a4264e72a0
dmgfhc <44460588@qq.com>
2017-12-05 15:05:31 +0800

    20171205下午更新

--- myeclipsews/MyProject/src/cn/mldn/string/StringDemo.java
+++ myeclipsews/MyProject/src/cn/mldn/string/StringDemo.java
@@ -4,12 +4,100 @@ public class StringDemo {
 
 	public static void main(String[] args) {
 		
-		String stra = new String("hello").intern();
+//		String stra = new String("hello").intern();
+//		
+//		String strb = "hello";
+//		
+//		System.out.println(stra==strb);
+//		String str = "hello";
+//		char c = str.charAt(0);
+//		System.out.println(c);
+//		
+//		String str = "123a423432";
+//		
+//		if(isNumber(str)) {
+//			System.out.println("字符由字符串组成");
+//		}else{
+//			System.out.println("字符由非数字组成");
+//		}
+//		
+//	}
+//	
+//	public static boolean isNumber(String temp) {
+//		
+//		char[] data = temp.toCharArray();
+//		for(int x=0;x<data.length;x++) {
+//			if(data[x]>'9' || data[x] <0) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+	
+//	String str = "helloworld";
+//	byte[] data = str.getBytes();
+//	for(int x=0;x<data.length;x++) {
+//		data[x]-=32;
+//	}
+//	System.out.println(new String(data));
+//	System.out.println(new String(data,5,5));
+	
+//	String stra = "Hello";
+//	String strb = "hELLO";
+//	System.out.println(stra.equals(strb));
+//	System.out.println(stra.equalsIgnoreCase(strb));
+	
+//	
+//	String str = "helloworld";
+//	
+//	System.out.println(str.indexOf("world"));
+//	
+//	System.out.println(str.indexOf("l"));
+//	
+//	System.out.println(str.indexOf("l",5));
+//	
+//	System.out.println(str.lastIndexOf("l"));
 		
-		String strb = "hello";
+//	String str = "##@@hello**";
+//	System.out.println(str.startsWith("##"));
+//	System.out.println(str.startsWith("@@",2));
+//	System.out.println(str.endsWith("**"));
+	
+//	String str = "helloworld";
+//	String resultA = str.replaceAll("l", "_");
+//	String resultB = str.replaceFirst("l", "_");
+//	System.out.println(resultA);
+//	System.out.println(resultB);
 		
-		System.out.println(stra==strb);
+//	String str = "helloworld";
+//	String resultA = str.substring(5);
+//	String resultB = str.substring(0, 5);
+//	System.out.println(resultA);
+//	System.out.println(resultB);
+		
+//	
 
+//		
+//	String str = "192.168.1.2";
+//	String result[] = str.split("\\.");
+//	for(int x=0;x<result.length;x++) {
+//		System.out.println(result[x]);
+//	}
+		
+//	
+//	String stra = "hello";
+//	String strb = stra + "world";
+//	String strc = "hello world";
+//	String strd = "hello" + " world";
+//	System.out.println(stra == strc);
+//	System.out.println(strb == strc);
+//	System.out.println(strc == strd);
+		
+	String str = "(*(*Hello(*(*";
+	System.out.println(str.toUpperCase());
+	System.out.println(str.toLowerCase());
+		
+		
+		
 	}
-
 }
